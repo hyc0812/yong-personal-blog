5096121fb96cee43b8c8f18a84007f1b35d1e04d
yongchang he <hyc0812@163.com>
2022-05-03 15:11:53 -0600

    first commit

---
+++ dojo-blog/src/Home.js
@@ -0,0 +1,40 @@
+import {useState, useEffect} from 'react';
+import BlogList from './BlogList';
+
+const Home = () => {
+
+    const [blogs, setBlogs] = useState(null);
+    const [isPending, setIsPending] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        setTimeout(() => {
+            fetch('http://localhost:8000/blogs')
+                .then(res => {
+                    if (!res.ok) {
+                        throw Error('could not fetch the data for that resource');
+                    }
+                    return res.json();
+                })
+                .then(data => {
+                    setBlogs(data);
+                    setIsPending(false);
+                    setError(null);
+                })
+                .catch(err => {
+                    setIsPending(false);
+                    setError(err.message);
+                })
+        }, 600); 
+        }, []);
+
+    return ( 
+      <div className="home">
+          { Error && <p>{error}</p> }
+          { isPending && <div>Loading...</div> }
+          {blogs && <BlogList blogs={ blogs } title="All Blogs" />}
+      </div>  
+     );
+}
+ 
+export default Home;

eee1dc959f3a09d9f291e42ef2beb4fa8098b5c7
yongchang he <hyc0812@163.com>
2022-05-04 11:28:02 -0600

    submit component added

--- dojo-blog/src/Create.js
+++ dojo-blog/src/Create.js
@@ -7,11 +7,24 @@ const Create = () => {
   const [body, setBody] = useState('');
   const [author, setAuthor] = useState('mario');
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const blog = { title, body, author };
+
+    fetch('http://localhost:8000/blogs', { 
+      method: 'POST',
+      headers: { "content-type": "application/json" },
+      body: JSON.stringify(blog)
+    }).then(() => {
+      console.log('new blog added');  
+    });
+  }
+
 
   return (
     <div className="create">
       <h2>Add a New Blog</h2>
-      <form>
+      <form onSubmit={ handleSubmit }>
         <label>Blog Title: </label>
         <input type="text"
           required 

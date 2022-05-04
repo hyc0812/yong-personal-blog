19fd3443c0f56b5c50506e541e6b556b10d544a2
yongchang he <hyc0812@163.com>
2022-05-04 12:10:21 -0600

    update

--- dojo-blog/src/Navbar.js
+++ dojo-blog/src/Navbar.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <h1>Personal Blog Demo</h1>
+      <h1>Blog Demo</h1>
       <div className="links">
         <Link to="/">Home</Link>
         <Link to="/create" style={{ 

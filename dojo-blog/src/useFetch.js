884ca13ae0938590427b3dc34e08c4aeeb84d322
Yongchang He <80454212+hyc0812@users.noreply.github.com>
2022-05-03 23:01:37 -0600

    updated

--- dojo-blog/src/useFetch.js
+++ dojo-blog/src/useFetch.js
@@ -1,13 +1,17 @@
 import { useState, useEffect  } from "react";
 const useFetch = (url) => {
 
+    
     const [data, setData] = useState(null);
     const [isPending, setIsPending] = useState(true);
     const [error, setError] = useState(null);
 
     useEffect(() => {
+
+        const abortCont = new AbortController();
+
         setTimeout(() => {
-            fetch(url)
+            fetch(url, { signal: abortCont.signal })
                 .then(res => {
                     if (!res.ok) {
                         throw Error('could not fetch the data for that resource');
@@ -20,15 +24,16 @@ const useFetch = (url) => {
                     setError(null);
                 })
                 .catch(err => {
-                    setIsPending(false);
-                    setError(err.message);
+                    if (err.name === 'AbortError') {
+                        console.log('fetch aborted');
+                    }else{
+                        setIsPending(false);
+                        setError(err.message);
+                    }
                 })
         }, 600); 
 
-        // return () => {
-        //     // cleanup
-        //     console.log('cleaning up');
-        // }   
+        return () => abortCont.abort(); 
     }, [url]);
 
     return { data, isPending, error };
